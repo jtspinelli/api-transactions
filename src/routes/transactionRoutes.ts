09bce1f7b49f4a71908ed2e7a8625472d40becbc
jtspinelli <contato@jonathanspinelli.com>
2023-03-19 17:11:49 -0300

    correct GetTransactionById route

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -5,7 +5,7 @@ const transactionRouter = Router();
 const transactionController = new TransactionController();
 
 transactionRouter.post('/user/:userId/transactions', transactionController.Create);
-transactionRouter.get('/user/:userId/:id', transactionController.GetById);
+transactionRouter.get('/user/:userId/transactions/:id', transactionController.GetById);
 transactionRouter.get('/users/:userId/transactions', transactionController.GetAllTransactionsFromUser);
 transactionRouter.delete('/users/:userId/transactions/:id', transactionController.Destroy);
 transactionRouter.put('/users/:userId/transactions/:id', transactionController.Update);
